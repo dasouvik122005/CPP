4158c3150a5e80dd2f6aaa5de7cd5f9e29925455
Souvik Das <dasouvik122005@gmail.com>
2025-08-18 12:12:40 +0530

    Add files via upload

---
+++ cls_obj.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+// class defination
+class student {
+private:
+    string name;
+    int rollNo;
+    float marks;
+
+public:
+    void setData(string n, int r, float m){
+        name = n;
+        rollNo = r;
+        marks = m;
+    }
+    // access speifier are three type- public, private, protected
+        //public: all
+        //private: only one 
+        //protected: few
+    // method
+    // method to display student info
+    void displayData() {
+        cout << "Name: " << name << endl;
+        cout << "Roll No: " << rollNo << endl;
+        cout << "Marks: " << marks << endl;
+    }
+};
+int main(){
+    student s1, s2, s3;
+    s1.setData ("Rahul",1,85.6);
+    s2.setData ("Ratul",2,89.6);
+
+    s1.displayData();
+    cout << endl;
+    s2.displayData();
+
+    return 0;
+}
